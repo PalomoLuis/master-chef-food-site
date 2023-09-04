815065a3a881aaa87ea5eff06fea5d3a420f3696
Luis Carlos Palomo <42524903+PalomoLuis@users.noreply.github.com>
2023-09-04 18:12:25 -0300

    images in public

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,17 +3,12 @@ import Layout from '@components/Layout/index.jsx'
 import Hero from '@components/Hero/index.jsx'
 import TitleDivider from '@components/Dividers/TitleDivider.jsx'
 import mainLogo from '@public/LogoMasterChef.svg'
-import HeroImage from '@images/vegetables-cheese-black-plate.jpeg'
 import { GalleryTwo, GalleryThree } from '@components/Sections/Gallery.jsx'
-import bgSection1 from '@images/rock-texture.jpeg'
 import { Title3 } from '@components/Text/Titles.jsx'
 import favourites from '@data/favoutires.js'
 import { TextCard, ImageCard, PromotionFullCard, FiftyYearsSpecialCard } from '@components/Cards/index.jsx'
-import favofitePlateImage from '@images/ekaterina-bolovtsova.jpeg'
 import products from '@data/products.json'
-import pizzaImage from '@images/pizzas-1.jpeg'
 import Footer from '@components/Sections/Footer.jsx'
-import fiftyYearsImage from '@images/fifty-years-chef-cooking.jpeg'
 import { CartOrder } from '@components/CartOrder/index.jsx'
 import { ShoppingCartContext } from '@context/index.jsx'
 
@@ -35,10 +30,10 @@ function Home() {
   return (
     <>
       <Layout>
-        <Hero bgImage={ HeroImage }/>
+        <Hero bgImage={ 'images/vegetables-cheese-black-plate.jpeg' }/>
         <TitleDivider title={ 'take a look at our menu' }/>
         <GalleryThree
-          bgImage={ bgSection1 }
+          bgImage={ 'images/rock-texture.jpeg' }
           elements={ favourites().map(value => {
             return (
               <ImageCard
@@ -46,7 +41,7 @@ function Home() {
                 title={ value?.name }
                 price={ value?.price }
                 cta='add'
-                image={ value?.image[0]}
+                image={ `images/${value?.image[0]}`}
                 key={ value?.id }
               />
             )
@@ -56,7 +51,7 @@ function Home() {
         </GalleryThree>
         <PromotionFullCard
           id={ 999 }
-          image={ favofitePlateImage }
+          image={ 'images/ekaterina-bolovtsova.jpeg' }
           preTitle="special of the day"
           title="Ekaterina bolovtsova"
           description="Sauteed portobellos with aromas of rosemary, accompanied with cornbread with apple tints."
@@ -82,9 +77,9 @@ function Home() {
         >
           <Title3 color={'text-white'} extraCss={'mb-12 lg:mb-16 text-center'}>Pizzas</Title3>
         </GalleryTwo>
-        <img src={ pizzaImage } alt="pizzas" className='h-[400px] lg:h-auto object-cover'/>
+        <img src={ 'images/pizzas-1.jpeg' } alt="pizzas" className='h-[400px] lg:h-auto object-cover'/>
         <FiftyYearsSpecialCard
-          image={ fiftyYearsImage }
+          image={ 'images/fifty-years-chef-cooking.jpeg' }
           title1='50'
           title2='years'
           description='The true teacher is born with time. Every year is a mark and an experience. However, all of this only makes us love what we do even more.'
